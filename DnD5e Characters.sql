5e7dc32291a455436d46d5d8d8ca0bf011af4c55
Nyever <114325799+nyever@users.noreply.github.com>
2024-01-07 07:32:03 +1100

    Add files via upload

---
+++ DnD5e Characters.sql
@@ -0,0 +1,14 @@
+CREATE table DnD5eCharacters
+(
+    Player_Name char(255)
+    Race char(255),
+    Class char(255),
+    Strength int, 
+    Dexterity int, 
+    Constituiton int, 
+    Intelligence int, 
+    Wisom int, 
+    Charisma int
+);
+INSERT INTO DnD5e_Characters(race, class, Strength, Dexterity, Constituiton, Intelligence, Wisom, Charisma)
+VALUES("Tabaxi", "Rogue", 9, 18, 16, 10, 14, 16);
