d6f47e6ede93056fefd4c697b24ba16994ff1a2a
Nyever <114325799+nyever@users.noreply.github.com>
2024-04-26 14:19:39 +1000

    Update Ball_Drop_Height.sql
    Fixed some errors and added some code

--- Ball_Drop_Height.sql
+++ Ball_Drop_Height.sql
@@ -6,25 +6,26 @@ run3_height,
 run4_height,
 PRIMARY KEY(ball_type)
 );
-INSERT INTO Data (ball_type, run, height)
+INSERT INTO Data (ball_type, run1_height, run2_height, run3_height, run4_height)
 VALUES
 ('Tennis Ball', 60, 59, 60, 60),
 ('PingPong Ball',49, 51, 54, 56),
 ('Golf Ball', 37, 42, 38, 40),
 ('Hand Ball', 74, 78, 78, 75);
 
---Sorting algorithm
 SELECT
---first_name as "Name",
 ball_type as "Ball Type", 
-run1_height as "Run 1"
-run2_height as "Run 2"
-run3_height as "Run 3"
-run4_height as "Run 4"
-(run1_height + run2_height + run3_height + run4_height)/4 as "Average Height"
-SQRT(run1_height)/10 as "Elastic Ratio 1"
-SQRT(run2_height)/10 as "Elastic Ratio 2"
-SQRT(run3_height)/10 as "Elastic Ratio 3"
-SQRT(run4_height)/10 as "Elastic Ratio 4"
+run1_height as "Run 1",
+run2_height as "Run 2",
+run3_height as "Run 3",
+run4_height as "Run 4",
+(run1_height + run2_height + run3_height + run4_height)/4 as "Average Height",
+SQRT(run1_height)/10 as "Elastic Ratio 1",
+SQRT(run2_height)/10 as "Elastic Ratio 2",
+SQRT(run3_height)/10 as "Elastic Ratio 3",
+SQRT(run4_height)/10 as "Elastic Ratio 4",
+(SQRT(run1_height + run2_height + run3_height + run4_height)/20) as "Average Elastic Ratio"
+  
 FROM data
 GROUP BY ball_type
+
