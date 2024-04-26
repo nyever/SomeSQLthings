3d9707f7c961f52e6cbf07d9fdf29f40f391f40f
Nyever <114325799+nyever@users.noreply.github.com>
2024-04-26 15:43:28 +1000

    Update Ball_Drop_Height.sql
    more fixing

--- Ball_Drop_Height.sql
+++ Ball_Drop_Height.sql
@@ -1,5 +1,5 @@
 CREATE TABLE data ( 
-ball_type VARCHAR(50)
+ball_type VARCHAR(50),
 run1_height,
 run2_height,
 run3_height,
@@ -13,6 +13,7 @@ VALUES
 ('Golf Ball', 37, 42, 38, 40),
 ('Hand Ball', 74, 78, 78, 75);
 
+
 SELECT
 ball_type as "Ball Type", 
 run1_height as "Run 1",
@@ -20,12 +21,11 @@ run2_height as "Run 2",
 run3_height as "Run 3",
 run4_height as "Run 4",
 (run1_height + run2_height + run3_height + run4_height)/4 as "Average Height",
-SQRT(run1_height)/10 as "Elastic Ratio 1",
-SQRT(run2_height)/10 as "Elastic Ratio 2",
-SQRT(run3_height)/10 as "Elastic Ratio 3",
-SQRT(run4_height)/10 as "Elastic Ratio 4",
-(SQRT(run1_height + run2_height + run3_height + run4_height)/20) as "Average Elastic Ratio"
-  
+--Round(x,y) rounds x to y decimal places  
+ROUND((SQRT(run1_height)/10), 2) as "Elastic Ratio 1",
+ROUND((SQRT(run2_height)/10), 2) as "Elastic Ratio 2",
+ROUND((SQRT(run3_height)/10), 2) as "Elastic Ratio 3",
+ROUND((SQRT(run4_height)/10), 2) as "Elastic Ratio 4",
+ROUND((SQRT(run1_height + run2_height + run3_height + run4_height)/20), 2) as "Average Elastic Ratio"
 FROM data
 GROUP BY ball_type
-
