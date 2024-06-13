70ce6905843ce0c090b1939ba8294c561046b307
Nyever <114325799+nyever@users.noreply.github.com>
2024-06-13 12:05:52 +1000

    Update DnD5e Characters.sql

--- DnD5e Characters.sql
+++ DnD5e Characters.sql
@@ -3,7 +3,8 @@ CREATE table DnD5e_Characters
 (
     Player_Name char(255),
     Char_Name char(255),
-    Race char(255),
+    Char_Race char(255),
+    Char_Gender char(255),
     Level int,
     Class char(255),
     Strength int, 
@@ -15,9 +16,9 @@ CREATE table DnD5e_Characters
 );
 
 --Setup for adding characters
-INSERT INTO DnD5e_Characters(Player_Name, Char_Name, Race, Class, Level, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma)
+INSERT INTO DnD5e_Characters(Player_Name, Char_Name, Char_Race, Char_Gender, Class, Level, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma)
 VALUES --Add characters here
-    ("Nyever", "Lotus", "Tabaxi", "Rogue", 1, 9, 18, 16, 14, 10, 16); --for example here is a character I made recently
+    ("Nyever", "Lotus", "Tabaxi", "Female", "Rogue", 1, 9, 18, 16, 14, 10, 16); --for example here is a character I made recently
 
 --Code for whatever you want to find out
-SELECT Race, Class FROM DnD5e_Characters;
+SELECT Player_Name, Class FROM DnD5e_Characters;
